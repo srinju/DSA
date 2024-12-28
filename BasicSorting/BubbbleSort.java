51cd345c8c931832e0302124477377bc9b749594
SRINJOY DAS <srinjoydas1104@gmail.com>
2024-12-28 18:56:20 +0530

    comms

--- BasicSorting/BubbbleSort.java
+++ BasicSorting/BubbbleSort.java
@@ -3,8 +3,8 @@ package BasicSorting;
 public class BubbbleSort {
     public static int[] bubble(int[] arr){
         int n = arr.length;
-        for(int pass = 0 ; pass < n ; pass++){
-            for(int i = 0 ; i+1 < n ; i++){
+        for(int pass = 0 ; pass < n ; pass++){ //do that for number of times
+            for(int i = 0 ; i+1 < n ; i++){ //thsi one does sorting once
                 //if the next value is greater than the present value then perform swapping , so swapping korte korte bora mal pechone chole jabe
                 if(arr[i+1] < arr[i]){
                     //swapping >
