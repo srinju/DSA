6254c4edcae2f5e146cebd3f9165821cb00d8eeb
SRINJOY DAS <srinjoydas1104@gmail.com>
2024-12-28 22:08:07 +0530

    insertion sort

--- BasicSorting/AllSorting.java
+++ BasicSorting/AllSorting.java
@@ -30,18 +30,41 @@ public class AllSorting {
         //then {1 , 2 , 5 ,4, 3}
         //we can see that the i is sorted part
         //and we dont want to do the nth sortng coz it will be already sorted so out loop runs till n-1
-        //and we run two loop one for i = 0 to n-1 and one for j = i+1 to n-1
+        //and we run two loop one for i = 0 to n-1 and one for j = i+1 to n
         //and assign the smallest value to i
         //when we see that any element in that array that is smaller than ith element then we make the smallest as that j
 
         int n  = arr.length;
         for(int i = 0 ; i < n-1 ; i++){
             int smallest = i;
-            for(int j = i+1 ; j < n-1 ; j++){
+            for(int j = i+1 ; j < n ; j++){
                 if(arr[j] < arr[smallest]){
                     smallest = j;
                 }
             }
+            //now as the smallest has become the jth index then we will have to swap the smallest value with that new smallest 
+            //swap>            
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+    public static int[] Insertion(int[] arr){
+
+        //pick an element from the unsorted part and place in the right position in the sorted part >
+        
+        int n = arr.length;
+        for(int i = 1 ; i < n ; i++){
+            int current = arr[i];
+            int prevoius = i-1;
+            while(prevoius >= 0 && arr[prevoius] > current){
+                arr[prevoius+1] = arr[prevoius]; //shift the element to right
+                prevoius--;
+            }
+            //insertion >>
+            arr[prevoius+1] = current; // fill the empty slot with the current value of the array
         }
         return arr;
     }
@@ -50,14 +73,15 @@ public class AllSorting {
         int n = arr.length;
         for(int i = 0 ; i < n ; i++){
             System.out.print(arr[i] + " ");
-        }
+        }   
         System.out.println();
     }
 
     public static void main(String[] args) {
-        int[] arr = {100,256 , 35 , 71 , 32 , 18 , 999 , 10 , 10  , 20};
-        printArr(bubble(arr)); //bubble sort
-        printArr(Selection(arr)); //selection sort
+        int[] arr = {8,2,4,1,3};
+        //printArr(bubble(arr)); //bubble sort
+        //printArr(Selection(arr)); //selection sort
+        printArr(Insertion(arr));
     }
 }
 
