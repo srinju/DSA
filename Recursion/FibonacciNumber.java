ce403a9acbef516b60af1a7e7fb673f6aee04b5e
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-01-01 02:02:41 +0530

    comms

--- Recursion/FibonacciNumber.java
+++ Recursion/FibonacciNumber.java
@@ -7,6 +7,9 @@ public class FibonacciNumber {
 
     //call stack will be like from low to up it will go like branches where n-1 and n-2 runs and gets
     //and while coming down it adds up the branches values and returns it
+    //and when two or more recursive functions are ran then the call stack can be represeneted as tree
+    //time complexity is exponenetial coz we can see prottek ta level of tree te number of calls double hocche .
+    //there for time complexity is O(2^n)
     
     public static int fib(int n){
         //base case >
