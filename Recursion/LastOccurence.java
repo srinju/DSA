911e13b4db9b95e5fd1d17d437297cbd62b850b0
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-01-01 17:48:01 +0530

    comms

--- Recursion/LastOccurence.java
+++ Recursion/LastOccurence.java
@@ -14,10 +14,10 @@ public class LastOccurence {
         return lastOccur(arr, key , i-1); //we compare the recursive part for the rest of the array which includes the second last element as we are travelling in reverse
     }
     public static void main(String[] args) {
-        int[] arr = {8,3,6,9,5,10,2,5,3};
+        int[] arr = {5,5,5,5};
         int n = arr.length;
         int i = n-1; //we sstart from the last index
-        int key = 3;
+        int key = 5;
         System.out.println("the last occurence of " + key + "is : " + lastOccur(arr, key , i));
     }
 }
