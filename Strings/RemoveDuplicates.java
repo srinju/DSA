1697c84568aafde7a3c981fa7f582df061214239
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-01-01 19:34:48 +0530

    adv

--- Strings/RemoveDuplicates.java
+++ Strings/RemoveDuplicates.java
@@ -9,13 +9,17 @@ public class RemoveDuplicates {
         StringBuilder sb = new StringBuilder();
         for(int i = 0 ; i < n ; i++){ //traverse throight the loop
             if(i==0 || str.charAt(i) != str.charAt(i-1)){ //if the first element is 0 then append the first element and check that the previous element is not equlas to the current element 
-                sb.append(str.charAt(i)); //append the element to the str builder
+                if(sb.toString().indexOf(str.charAt(i)) == -1){ //check if the current element already exisits in the str builder or not
+                    sb.append(str.charAt(i)); //append the element to the str builder
+                }  
             }
         }
         return sb.toString(); //return the string builder by converting it into a string
     }
     public static void main(String[] args) {
-        String str = "aaabbbccc";
-        System.out.println("the string after removing duplicates is : " + removeCommon(str));
+        String str1 = "aabbbccc"; //remember the test case would fell for appa as we are checling only the concequtive ones ... for appa kind of test case we need hash set which will be done later 
+        String str2 = "appappa";
+        System.out.println("the string aabbbccc after removing duplicates is : " + removeCommon(str1));
+        System.out.println("the string appappa after removing duplicates is : " + removeCommon(str2));
     }
 }
