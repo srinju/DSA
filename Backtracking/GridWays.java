4ad8b4056ff3c7e1503f9ffb9956335096dbf78a
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-01-07 03:29:31 +0530

    asd

---
+++ Backtracking/GridWays.java
@@ -0,0 +1,15 @@
+package Backtracking;
+
+public class GridWays {
+    
+    //PROBLEM STATEMENT>
+
+    //Find number of ways to reach from (0,0) to (n-1 , m-1) in a n*m grid . 
+    //Allowed moves is right or down
+
+    //APPROACH>>
+
+    //we can see that all the places have two kind of moves right or down except the most right ones on each row which has only down move and the most left bottom one which has only right move and no down move
+    //
+
+}
