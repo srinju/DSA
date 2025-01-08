420f021c725cdad592e0567108372d6154196aaf
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-01-08 17:33:39 +0530

    max no

--- ArrayLists/FindMax.java
+++ ArrayLists/FindMax.java
@@ -5,24 +5,23 @@ import java.util.ArrayList;
 public class FindMax {
     public static int findMax(ArrayList<Integer> list){
         int max = Integer.MIN_VALUE;
-        for(int i = 0 ; i < list.size()-1 ; i++){
-            for(int j = i+1 ; j<list.size() ; j++){
-                if(list.get(i) > list.get(j)){
-                    max = list.get(i);
-                } else {
-                    max = list.get(j);
-                }
+        for(int i = 0 ; i<list.size() ; i++){
+            /* 
+            if(max < list.get(i)){
+                max = list.get(i);
             }
+                */
+            max = Math.max(max, list.get(i));
         }
         return max;
     }
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
         list.add(2);
-        list.add(3);
-        list.add(4);
         list.add(5);
+        list.add(9);
+        list.add(6);
+        list.add(8);
         System.out.println("the max number in the array list is : " + findMax(list));
     }
 }
