bb436d26e31cd8c7bb3a50cdb18f4988438c1856
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-01-18 18:36:55 +0530

    reverse node in form of k groups

--- LinkedLists/ReverseNodesInKGroup.java
+++ LinkedLists/ReverseNodesInKGroup.java
@@ -57,7 +57,71 @@ public class ReverseNodesInKGroup {
         //recurision gives us the head of the next reversed
         //we add our newTail with the reversed head and it is done
 
+        //base case >>
+        if(head == null){
+            return null;
+        }
         
+        //checking if a current group has k elements or not
+        Node temp = head ; 
+        int count = 0 ;
+
+        while(temp != null && count < k){
+            temp = temp.next;
+            count++;
+        }
+
+        if(count < k){
+            return head;
+        }
+
+        //perform the first grp reverse 
+        Node prev = null;
+        Node current = head;
+        count = 0 ;
+        Node next = null;
+        //perform the first group reverse >>
+
+        while(current != null && count < k){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            count++;
+        }
+
+        //after the while loop ends it returns the prev as the head and the next as the head of the new group
+
+        //recursively reverse the next LIst>
+
+        if(next != null){
+            //recursion call returns the newHead of the next reversed group >
+            Node newHead = reverseInKGroup(next, k);
+            head.next = newHead; //connect the head tht is the tail of the prev reversed group with the head of the newly reversed grp
+        }
+
+        //the prev is the newHead of the first group
+
+        return prev;
+
+        
+    }
+
+    public static void main(String[] args) {
+        
+        ReverseNodesInKGroup ll = new ReverseNodesInKGroup();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        ll.printList();
+
+        ll.head = ll.reverseInKGroup(ll.head, 2);
+
+        ll.printList();
     }
 
 }
