9169076900ac42d14a00b36f00c583d6a9d72f92
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-05-13 09:35:26 +0530

    corrected code

--- FAT/twoItems.java
+++ FAT/twoItems.java
@@ -1,51 +1,30 @@
-
 package FAT;
 import java.util.*;
 
 public class twoItems {
-    
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-        //no of people>
-        System.out.println("enter the no of people");
-        int n = sc.nextInt();
-        sc.nextLine();
-        //enter items
-        System.out.println("enter the list of items with space");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter no of peeps");
+        int k = sc.nextInt();
+        sc.nextLine(); // clear buffer
+
+        System.out.println("enter the items");
         String[] items = sc.nextLine().split(" ");
 
-        //each person is a map of item ->. count
-        List<Map<String,Integer>> poeple = new ArrayList<>();
-        for(int i = 0 ; i < n ; i ++){
-            poeple.add(new HashMap<>());
-        }
+        Map<String, Integer> count = new HashMap<>();
+        for (String item : items)
+            count.put(item, count.getOrDefault(item, 0) + 1);
 
-        int index = 0 ; //curr person index
-        for(String item : items){
-            boolean given = false;
-            //try giving item to next n poeple>
-            for(int i = 0 ; i < n ; i ++){
-                Map<String,Integer> person = poeple.get(index);
-                int count  = person.getOrDefault(item, 0);
-
-                if(count < 2){
-                    person.put(item , count + 1);
-                    given = true;
-                    break;
-                }
-
-                index = (index + 1) % n;
-            }
-
-            if(!given) {
-                System.out.println("could not assign : " + item);
+        boolean possible = true;
+        for (int freq : count.values()) {
+            if (freq > 2 * k) {
+                possible = false;
+                break;
             }
         }
 
-        for(int i = 0 ; i < n ; i ++) {
-            System.out.println("person " + (i+1) + ": " + poeple.get(i));
-        }
-
+        System.out.println("dist poss ? " + possible);
         sc.close();
     }
 }
